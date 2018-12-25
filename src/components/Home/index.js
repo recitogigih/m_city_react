ba651eb426201e8c1994f76bacea3a3bf0ba9a0b
recitogigih <recitogigih9@gmail.com>
2018-12-26 01:54:39 +0700

    meet the player section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Featured from './Featured/index'
 import Matches from './Matches'
+import MeetPlayers from './meetPlayers'
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Featured/>
             <Matches/>
+            <MeetPlayers/>
         </div>
     );
 };
