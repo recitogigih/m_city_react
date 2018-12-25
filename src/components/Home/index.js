abf057c04fd58f37685266d8b7c1a97d154da090
recitogigih <recitogigih9@gmail.com>
2018-12-25 23:38:52 +0700

    making tag (button)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Featured from './Featured/index'
+import Matches from './Matches'
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Featured/>
+            <Matches/>
         </div>
     );
 };
