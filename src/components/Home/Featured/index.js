2c9ad894a3e08653a68fe778f932d8c573620165
recitogigih <recitogigih9@gmail.com>
2018-12-25 22:43:56 +0700

    animation layer 2

--- src/components/Home/Featured/index.js
+++ src/components/Home/Featured/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Stripes from './stripes'
+import Stripes from './stripes';
+import Text from './text';
 const Featured = () => {
     return (
         <div className="featured_wrapper">
             <Stripes/>
+            <Text/>
         </div>
     );
 };
